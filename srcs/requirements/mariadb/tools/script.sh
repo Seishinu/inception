163d47e5f0b0337fb2f1fdb76aea4900f140f94d
SeishinU <yassinnuiga1616@gmail.com>
2023-08-11 14:51:36 +0100

    close

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,8 +1,6 @@
-
-#!bin/bash
+#!/bin/bash
 
 service mysql start
-
 echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;" > mydb.sql
 echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> mydb.sql
 echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;" >> mydb.sql
@@ -14,4 +12,4 @@ service mysql stop
 
 mysqld --bind-address=0.0.0.0
 
-mysqld_safe
+# mysqld_safe
